03f4c763d9c9067a450e9e58da254351b91341a0
Rabidgeek <admin@jesseragsdale.com>
2023-12-10 17:40:15 -0800

    start of lab assignments for coursera

---
+++ arrays.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var levelScores:[Int] = [ ]
+
+if levelScores.isEmpty {
+    print("Welcome to the game of HELL!!!")
+}
+
+let firstLevelScore = 10
+
+levelScores.append(firstLevelScore)
+
+print("The first level score is \(levelScores[0]).")
+
